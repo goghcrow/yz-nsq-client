ef26e368688d80e85fbd398b3b61c7c558eab0c4
xiezhenye <xiezhenye@qabb-r1buffer24.h.qima-inc.com>
2017-08-03 17:50:16 +0800

    fix test

--- test/boot.php
+++ test/boot.php
@@ -5,8 +5,18 @@ use Zan\Framework\Components\Nsq\Utils\Binary;
 use Zan\Framework\Components\Nsq\Utils\MemoryBuffer;
 use Zan\Framework\Components\Nsq\Utils\ObjectPool;
 use Zan\Framework\Foundation\Core\Debug;
+use ZanPHP\SPI\AliasLoader;
+use ZanPHP\SPI\ServiceLoader;
 
 require_once __DIR__ . "/../vendor/autoload.php";
+
+$vendor = __DIR__ . '/../vendor/';
+$aliasLoader = AliasLoader::getInstance();
+$aliasLoader->scan($vendor);
+
+$serviceLoader = ServiceLoader::getInstance();
+$serviceLoader->scan($vendor);
+
 Debug::detect();
 
 $config = require_once __DIR__ . "/config.php";
